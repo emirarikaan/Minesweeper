b1eb9f7876a2256cffaccf23275bb24144ae7865
emirarikaan <emirarikan99@gmail.com>
2022-06-14 21:30:09 +0300

    Minesweeper

---
+++ Mayin/src/MayinTarlasi.java
@@ -0,0 +1,153 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MayinTarlasi implements MouseListener {
+  JFrame frame;
+  Btn [][]board=new Btn[10][10];
+  private ImageIcon image1=new ImageIcon(getClass().getResource("ant.png"));  
+  private ImageIcon image2=new ImageIcon(getClass().getResource("flag.png"));  
+
+  public MayinTarlasi() {
+	  frame=new JFrame("Mayın Tarlası");
+	  frame.setSize(800,800);
+	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  frame.setLayout(new GridLayout(10,10));
+	  for(int row = 0;row<board.length;row++) {
+		  for(int col=0;col<board[0].length;col++) {
+			  Btn b = new Btn(row,col);
+			  frame.add(b);
+			  b.addMouseListener(this);
+			  
+			  board[row][col]=b;
+			  
+		  }
+	  }
+	  
+      generateMine();
+      uptadeCount();
+	//  print();
+	  
+
+	  frame.setVisible(true);
+  }
+  public void generateMine() {
+	  int i = 0 ;
+	  
+	  while(i<10) {
+	  int randRow=(int) (Math.random()*board.length);
+	  int randCol=(int) (Math.random()*board[0].length);
+      while(board[randRow][randCol].isMine()) {
+    	   randRow=(int) (Math.random()*board.length);
+    	   randCol=(int) (Math.random()*board[0].length);  
+  }
+      board[randRow][randCol].setMine(true);
+      i++;
+  }
+  }
+  public void print() {
+	  for(int row = 0;row<board.length;row++) {
+		  for(int col=0;col<board[0].length;col++) {
+			  if(board[row][col].isMine()) {
+				  board[row][col].setIcon(image1);
+			  }
+			  else {
+				  board[row][col].setText(board[row][col].getCount()+"");
+				  board[row][col].setEnabled(false);
+			  }
+		  }
+  }
+	  }
+  public void uptadeCount() {
+	  for(int row = 0;row<board.length;row++) {
+		  for(int col=0;col<board[0].length;col++) {
+			  if(board[row][col].isMine()) {
+				  counting(row,col);
+			  }
+		  }
+		  }
+  }
+  public void counting(int row,int col) {
+	  for(int i=row-1;i<=row+1;i++) {
+		  for(int k = col-1;k<=col+1;k++) {
+			  try {
+			  int value = board[i][k].getCount();
+			  board[i][k].setCount(++value);
+			  }
+			  catch(Exception e) {
+				  
+			  }
+		  }
+	  }
+  }
+  public void open(int r,int c) {
+	  if(r<0||r>=board.length||c<0||c>=board[0].length||board[r][c].getText().length()>0||board[r][c].isEnabled()==false) {
+		  return;
+	  }else if (board[r][c].getCount()!=0) {
+		  board[r][c].setText(board[r][c].getCount()+"");
+		  board[r][c].setEnabled(false);
+	  }
+	  else {
+		  board[r][c].setEnabled(false);
+		 
+		  open(r-1,c);
+		  open(r+1,c);
+		  open(r,c-1);
+		  open(r,c+1);
+	  }
+  }
+@Override
+public void mouseClicked(MouseEvent e) {
+	Btn b = (Btn) e.getComponent();
+	if(e.getButton()==1) {
+		System.out.println("sol");
+		if(b.isMine()) {
+			JOptionPane.showMessageDialog(frame,"KARINCAAA");
+			print();
+		}
+		else {
+			open(b.getRow(),b.getCol());
+		}
+		
+	}else if(e.getButton()==3) {
+		if(!b.isFlag()) {
+			b.setIcon(image2);
+			b.setFlag(true);
+		}
+		else {
+			b.setIcon(null);
+			b.setFlag(false);
+		}
+	}
+	
+}
+@Override
+public void mousePressed(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void mouseReleased(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void mouseEntered(MouseEvent e) {
+	// TODO Auto-generated method stub
+	Btn b = (Btn) e.getComponent();
+	b.setBackground(Color.ORANGE);
+	
+}
+@Override
+public void mouseExited(MouseEvent e) {
+	// TODO Auto-generated method stub
+	Btn b = (Btn) e.getComponent();
+	b.setBackground(null);
+	
+}
+  
+}
